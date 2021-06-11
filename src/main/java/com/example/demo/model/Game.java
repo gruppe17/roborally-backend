c1a6c836b6e559f86e6f883db0f6350ac5e3a199
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-06-11 14:33:42 +0200

    Game now has a gameId

--- src/main/java/com/example/demo/model/Game.java
+++ src/main/java/com/example/demo/model/Game.java
@@ -1,4 +1,12 @@
 package com.example.demo.model;
 
 public class Game {
+	private long gameId;
+
+	public long getGameId() {
+		return gameId;
+	}
+	public void setGameId(long gameId) {
+		this.gameId = gameId;
+	}
 }
