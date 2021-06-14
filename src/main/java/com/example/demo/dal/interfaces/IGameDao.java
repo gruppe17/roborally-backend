92615dcf5c67c46e7d6441c0d2bbbdcfa232522d
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-06-14 11:39:52 +0200

    (I)GameDao added getAllGames method

--- src/main/java/com/example/demo/dal/interfaces/IGameDao.java
+++ src/main/java/com/example/demo/dal/interfaces/IGameDao.java
@@ -3,9 +3,13 @@ package com.example.demo.dal.interfaces;
 import com.example.demo.exceptions.DaoException;
 import com.example.demo.model.Game;
 
+import java.util.List;
+
 
 public interface IGameDao {
 	Game getGame(long gameId) throws DaoException;
+
+	List<Game> getAllGames() throws DaoException;
 
 	long createGame(Game game) throws DaoException;
 
