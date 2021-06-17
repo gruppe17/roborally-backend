45f3d816fd64a6f47ac7cd9b2f7c6c3743943d87
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-06-17 11:20:56 +0200

    DTO mapper handles playerId

--- src/main/java/com/example/demo/util/mapping/DtoMapper.java
+++ src/main/java/com/example/demo/util/mapping/DtoMapper.java
@@ -130,8 +130,13 @@ public class DtoMapper implements IDtoMapper {
 		if (board == null) { //Incase the provided board id is invalid
 			throw new MappingException("Board was null when trying to convert PlayerDto to Player");
 		}
+
 		if (playerDto.getPlayerId() == null) { //If we have not provided a player id, we are creating a new player
 			return new Player(board, playerDto.getPlayerColor(), playerDto.getPlayerName());
+		}
+		else{
+		Player player = new Player(board, playerDto.getPlayerColor(), playerDto.getPlayerName());
+		player.setPlayerId(playerDto.getPlayerId());
 		}
 		return null;
 	}
